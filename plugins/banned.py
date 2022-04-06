04721a5e1a84d13df645223c82aac0345c458274
Desi BotZ <100966393+advfilter@users.noreply.github.com>
2022-04-06 12:00:31 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -25,7 +25,7 @@ async def ban_reply(bot, message):
 @Client.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
     buttons = [[
-        InlineKeyboardButton('𝙾𝚆𝙽𝙴𝚁', url=f'https://t.me/{SUPPORT_CHAT}')
+        InlineKeyboardButton('𝙾𝚆𝙽𝙴𝚁', url=f'https://t.me/BKC00bot')
     ]]
     reply_markup=InlineKeyboardMarkup(buttons)
     vazha = await db.get_chat(message.chat.id)
